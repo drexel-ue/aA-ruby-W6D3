e877307ce46f53281a618a303e8dd525b70b8ed7
ikesh pack <packappsforyou@gmail.com>
2019-05-29 13:28:59 -0700

    Add attr_reader for employee

--- class_inheritance/manager.rb
+++ class_inheritance/manager.rb
@@ -2,6 +2,8 @@ require_relative 'employee'
 
 class Manager < Employee
 
+    attr_reader :name, :title, :salary, :boss
+
     def initialize(name, title, salary, boss)
         super(name, title, salary, boss)
         @employees = []
